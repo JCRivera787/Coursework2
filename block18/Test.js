2c1412ec19fd3c237a194e4b5c19db57f10d4905
JCRivera787 <jeancapsp@yahoo.com>
2023-05-08 14:50:19 -0500

    block 18 test done

---
+++ block18/Test.js
@@ -0,0 +1,65 @@
+/*Prompt 3:
+Write functional tests for a shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+
+Expectations:
+
+The checkout feature should allow the user to check out as a guest or a logged-in user.
+The feature should prompt the user to create an account or log in if they check out as a guest.
+The feature should handle the case where the cart is empty.
+The feature should display the correct information at each step of the checkout process.
+Specifications:
+
+The user should be able to successfully check out as a guest and complete the order.
+The user should be able to successfully log in and complete the order.
+The user should be prompted to create an account or log in if they check out as a guest.
+The user should be prompted to enter their shipping and billing information.
+The user should be shown a summary of their order before completing the purchase.
+The user should be able to cancel the purchase before completing the order.
+The user should be able to go back and edit their shipping and billing information.*/
+
+
+
+/*Prompt 2:
+Write a function called concatOdds that takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+
+Expectations:
+
+The concatOdds function should return an array containing only odd numbers in ascending order.
+The function should handle different combinations of odd and even numbers correctly.
+The function should handle empty input arrays correctly.
+The function should handle repeated odd numbers correctly.
+Specifications:
+
+The function should return an empty array if both input arrays are empty.
+The function should return an empty array if both input arrays contain only even numbers.
+The function should return an array containing only the odd numbers from the first input array if the second input array is empty.
+The function should return an array containing only the odd numbers from the second input array if the first input array is empty.
+The function should return an array containing only the unique odd numbers from both input arrays.
+The function should return an array containing repeated odd numbers correctly.
+The function should return an array containing only one odd number if both input arrays contain only one odd number.
+The function should return the odd numbers in ascending order.*/
+
+/*Prompt 1 :
+Create a function named "multiplication" that multiplies two integers and returns their product. Write unit tests to ensure the function handles positive and negative integers, zero, and large numbers.
+
+Expectations:
+
+The multiplication function should return the correct product of its two input integer arguments.
+The function should handle positive and negative integers, zero, and large numbers correctly.
+
+Specifications:
+
+The function should return 0 if either input integer is 0.
+The function should return the correct product for two positive or negative integers.
+The function should return the correct product for a positive and a negative integer.
+The function should return the correct product for a positive integer and zero.
+The function should return the correct product for a negative integer and zero.
+The function should return the correct product for two large positive or negative integers.
+The function should return the correct product for a large positive integer and a small negative integer.
+The function should return the correct product for a small positive integer and a large negative integer.*/
+
+
+
+
+
+
